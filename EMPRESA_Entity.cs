7cde81b130b947dcdc136e3b76819222a7d17676
ALEJANDRA NOREÑA <anm22394@gmail.com>
2022-11-01 18:22:15 -0500

    Creacion de la capa modelo

---
+++ EMPRESA_Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class EMPRESA_Entity
+    {
+        public string CODEMPRESA { get; set; }
+        public string NIT { get; set; }
+        public string RAZONSOCIAL { get; set; }
+    }
+}
