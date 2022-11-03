5076084361dc1a1ea7b814923a6428b13763b462
ALEJANDRA NOREÑA <anm22394@gmail.com>
2022-11-02 20:40:12 -0500

    creacion de la clase zonas

---
+++ ZONAS_Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class ZONAS_Entity
+    {
+        public string CODIGO { get; set; }
+        public string DESCRIPCION { get; set; }
+        public bool PREDETERMINADO { get; set; }
+    }
+}
