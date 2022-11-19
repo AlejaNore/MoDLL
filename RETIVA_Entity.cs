fd93304856b469cfa2c36075f1aaa91f457f1493
ANDRY <orion.andry.toro@gmail.com>
2022-11-18 23:52:18 -0500

    MODELO RETIVA Y DET

---
+++ RETIVA_Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class RETIVA_Entity
+    {   
+        public string CODRETIVA { get; set; }
+        public string DESCRIPCION { get; set; }
+        public string FECODRFIVA { get; set; }
+        public string ACTIVO { get; set; }
+        public int Estado { get; set; }
+    }
+}
