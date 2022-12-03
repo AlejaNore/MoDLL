f4aebb6a24f839ae4285d2f50610dfa7244c43f3
ANDRY <orion.andry.toro@gmail.com>
2022-12-02 22:08:07 -0500

    CAPA MODELO PERPLANDES

---
+++ PERPLANDES_Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PERPLANDES_Entity
+    {
+        public string Tabla = "PERPLANDES";
+        public string CODEMPRESA { get; set; }
+        public int VERSION { get; set; }
+        public int ANO { get; set; }
+        public DateTime FECHA_INI { get; set; }
+        public DateTime FECHA_FIN { get; set; }
+        public int Estado { get; set; }
+    }
+}
