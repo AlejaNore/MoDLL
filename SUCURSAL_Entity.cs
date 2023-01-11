b63944188b90f8a801a4801345a8733d94904123
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-01-10 20:22:39 -0500

    cambios en SUCURSAL_Entity.cs

---
+++ SUCURSAL_Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class SUCURSAL_Entity
+    {
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public string RAZONSOCIAL { get; set; }
+        
+    }
+}
