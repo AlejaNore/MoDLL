891b829282381e7f4e13b3c1b2e145ad2dff33eb
ANDRY <orion.andry.toro@gmail.com>
2023-01-20 17:17:06 -0500

    modelo maestro meta

---
+++ PLAMAEMETA_Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PLAMAEMETA_Entity
+    {
+        public string Tabla = "PLAMAEMETA";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public string CODMETA { get; set; }
+        public string DESCRIPCION { get; set; }
+
+    }
+}
