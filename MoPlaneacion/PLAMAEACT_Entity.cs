8802e7f7caf93f5fa6e37a237147c8deb05e66b1
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-01-30 17:13:54 -0500

    Creacion de la clase PLAMAEACT_Entity.cs

---
+++ MoPlaneacion/PLAMAEACT_Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PLAMAEACT_Entity
+    {
+        public string Tabla = "PLAMAEACT";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public string CODACTIVIDAD { get; set; }
+        public string DESACTIVIDAD { get; set; }
+        public string CODACTIVIDAD_TEM { get; set; }
+    }
+
+}
