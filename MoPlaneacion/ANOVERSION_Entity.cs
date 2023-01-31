4eaa05745d8a4e55c90e933d871fc3d11c8dd502
ANDRY <orion.andry.toro@gmail.com>
2023-01-30 19:32:07 -0500

    modificacion año version

---
+++ MoPlaneacion/ANOVERSION_Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL.MoPlaneacion
+{
+    public class ANOVERSION_Entity
+    {       
+        public string Tabla = "PLAPERPDS";
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public int ANOINIVIG { get; set; }
+        public int VERSION { get; set; }                
+    }
+}
