b1abcff01eccbb0a28ddefc601adc22b0604c41d
ANDRY <orion.andry.toro@gmail.com>
2023-01-30 21:35:29 -0500

    CREACION DE MODELO AÑO Y VERSION

--- MoPlaneacion/ANOVERSION_Entity.cs
+++ MoPlaneacion/ANOVERSION_Entity.cs
@@ -12,6 +12,8 @@ namespace MoDLL.MoPlaneacion
         public string CODEMPRESA { get; set; }
         public string CODSUCURSAL { get; set; }
         public int ANOINIVIG { get; set; }
-        public int VERSION { get; set; }                
+        public int VERSION { get; set; }
+        public string ACTIVO { get; set; }
+
     }
 }
