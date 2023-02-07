12381effc018d09d896abc0dae1c18ccae5e7531
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-07 17:48:17 -0500

    Creacion de la capa MAEAREATRA_Entity.cs

---
+++ MoPlaneacion/MAEAREATRA_Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class MAEAREATRA_Entity
+    {
+        public string Tabla = "MAEAREATRA";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public string CODAREATRA { get; set; }
+        public string DESAREATRA { get; set; }
+        public string CODAREATRA_TEM { get; set; }
+    }
+}
