ac6eca9076bf5d2b96521d68a3e2c29627b0832b
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-08 16:52:56 -0500

    Creacion de la clase PLAHOMACTINDMETA_Entity.cs

---
+++ MoPlaneacion/PLAHOMACTINDMETA_Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PLAHOMACTINDMETA_Entity
+    {
+        public string Tabla = "PLAHOMACTINDMETA";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public int ANOINIVIG { get; set; }
+        public int VERSION { get; set; }
+        public int ANOPLANACC { get; set; }
+        public string CODPLANDES { get; set; }
+        public string CODEJEEST { get; set; }
+        public string CODPROGRAMA { get; set; }
+        public string CODPROYECTO { get; set; }
+        public string CODACTIVIDAD { get; set; }
+        public int CODINDICADOR { get; set; }
+        public double LINEABASE { get; set; }
+        public string CODMETA { get; set; }
+        public double CANTACT { get; set; }
+        public string CODRESULTADO { get; set; }
+        public string OBSERVMETA { get; set; }
+    }
+}
