b8a7b5d5c910b72caf33a2de51f9ea8ef7765d80
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-07 21:00:16 -0500

    Creacion de la clase PLAPLANACC_Entity.cs Y PERRUBRO_Entity.cs

---
+++ MoPlaneacion/PLAPLANACC_Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PLAPLANACC_Entity
+    {
+        public string Tabla = "PLAPLANACC";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public int ANOINIVIG { get; set; }
+        public int VERSION { get; set; }
+        public string CODPLANDES { get; set; }
+        public string CODEJEEST { get; set; }
+        public string CODPROGRAMA { get; set; }
+        public string CODPROYECTO { get; set; }
+        public int ANOPLANACC { get; set; }
+        public string CODACTIVIDAD { get; set; }
+        public double FACTPOND { get; set; }
+    }
+}
---
+++ MoPresupuesto/PERRUBRO_Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL.MoPresupuesto
+{
+    public class PERRUBRO_Entity
+    {
+        public string Tabla = "PERRUBRO";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public int ANORUBRO { get; set; }
+        public DateTime FECHA_INI { get; set; }
+        public DateTime FECHA_FIN { get; set; }
+        public string ACTIVO { get; set; }
+    }
+}
