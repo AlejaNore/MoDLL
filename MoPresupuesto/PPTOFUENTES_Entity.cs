d2700283718a4b8ac0b52fab624f5706251d44bb
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-09 12:26:09 -0500

    Se creo la clase PPTOFUENTES_Entity.cs

---
+++ MoPresupuesto/PPTOFUENTES_Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class PPTOFUENTES_Entity
+    {
+        public string Tabla = "PPTOFUENTES";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODIGO { get; set; }
+        public string NOMBRE { get; set; }
+        public string CON_FONDOS { get; set; }
+        public string LIBRE_DESTINACION { get; set; }
+        public string INVERSION { get; set; }
+        public string FUNCIONAMIENTO { get; set; }
+        public string CODIGOHOMOLOGADO { get; set; }
+        public string CODHOMOCHIP { get; set; }
+    }
+}
