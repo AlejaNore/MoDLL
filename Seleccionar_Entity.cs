969f35a1ad1f0083eb28f32c5386c60cd51521e6
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-10 11:03:19 -0500

    Creacion de la clase Seleccionar_Entity

---
+++ Seleccionar_Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class Seleccionar_Entity
+    {
+        public int NumSql { get; set; }
+        public string CODPARAMETRO1 { get; set; }
+        public string DESPARAMETRO1 { get; set; }
+        public string CODPARAMETRO2 { get; set; }
+        public string DESPARAMETRO2 { get; set; }
+
+    }
+}
