7d7f1b3cd04d8d1f3b64c0201883007696dad51a
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-02-14 19:53:05 -0500

    organizando el seleccionar

--- Seleccionar_Entity.cs
+++ Seleccionar_Entity.cs
@@ -9,7 +9,6 @@ namespace MoDLL
     public class Seleccionar_Entity
     {
         public int NumSql { get; set; }
-        public bool SeleccionMultiple { get; set; }
         public string CODPARAMETRO1 { get; set; }
         public string DESPARAMETRO1 { get; set; }
         public string CODPARAMETRO2 { get; set; }
