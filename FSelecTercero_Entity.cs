5e16f4134ea9ec3eaa589a1675b6fb65a86fe039
ANDRY <orion.andry.toro@gmail.com>
2023-02-21 19:40:45 -0500

    creacion de modelos de tercero y sucurter

---
+++ FSelecTercero_Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL
+{
+    public class FSelecTercero_Entity
+    {
+        public string Tabla = "TERCERO";
+        public string Tabla1 = "SUCURTER";
+
+        public string NITCC { get; set; }
+        public string DESCRIPCION { get; set; }
+        public string CODIGO { get; set; }
+        public string TEL1 { get; set; }
+        public string TEL2 { get; set; }
+        public string TEL3 { get; set; }
+        public string SUCURTER { get; set; }
+        public string RAZONSOCIAL { get; set; }
+        public string CLIENTE { get; set; }
+        public string PROVEEDOR { get; set; }
+        public string ACREEDOR { get; set; }
+        public string VENDEDOR { get; set; }
+        public string EMPLEADO { get; set; }
+        public string SOCIO { get; set; }
+        public string CONTRATISTA { get; set; }
+        public string OTROS { get; set; }
+    }
+}
---
+++ MoConfiguracion/SUCURTER_Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL.MoConfiguracion
+{
+    public class SUCURTER_Entity
+    {
+        public string Tabla = "SUCURTER";
+        public string NITCC { get; set; }
+        public string SUCURTER { get; set; }
+        public string RAZONSOCIAL { get; set; }
+        public string DIRECCION { get; set; }
+        public string TEL1 { get; set; }
+        public string TEL2 { get; set; }
+        public string TEL3 { get; set; }
+
+    }
+}
