44c7beeeec9d546ea5fa80ae4c10520cfab9d053
ANDRY <orion.andry.toro@gmail.com>
2023-03-07 18:32:41 -0500

    creacion maestro detalle plan desarrollo

---
+++ MoPlaneacion/MAESTRODET_PLAPDSNIV_Entity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL.MoPlaneacion
+{
+    public class MAESTRODET_PLAPDSNIV_Entity
+    {
+
+        public string Tabla = "PLAPDSNIV";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public int ANOINIVIG { get; set; }
+        public int VERSION { get; set; }
+        public int NIVEL { get; set; }
+        public string CODPLANDES { get; set; }
+        public string CODEJEEST { get; set; }
+        public string CODPROGRAMA { get; set; }
+        public string CODPROYECTO { get; set; }
+        public string DESCRIPCION { get; set; }
+        public double FACTPOND { get; set; }
+        public string CODPLANDES_TEM { get; set; }
+        public string CODEJEEST_TEM { get; set; }
+        public string CODPROGRAMA_TEM { get; set; }
+        public string CODPROYECTO_TEM { get; set; }
+
+        public List<DETALLE_PLAPDSNIV1_Entity> RefDetalle_PLANDESNIV1 { get; set; }
+
+        public MAESTRODET_PLAPDSNIV_Entity()
+        {
+            RefDetalle_PLANDESNIV1 = new List<DETALLE_PLAPDSNIV1_Entity>();
+        }
+    }
+
+    public class DETALLE_PLAPDSNIV1_Entity
+    {   //detalle
+            [Key]
+            public string Tabla = "PLAPDSNIV1";
+            public int Estado { get; set; }
+            public string CODEMPRESA { get; set; }
+            public string CODSUCURSAL { get; set; }
+            public int ANOINIVIG { get; set; }
+            public int VERSION { get; set; }
+            public string CODPLANDES { get; set; }
+            public string PROPOSITOGEN { get; set; } = string.Empty;
+            public string METODOLOGIA { get; set; } = string.Empty;
+            public string CODPLANDES_TEM { get; set; }           
+    }
+    
+}
