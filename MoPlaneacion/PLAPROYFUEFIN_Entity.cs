5196f34290406e8918cc75635ec5dac0f6e60800
ANDRY <orion.andry.toro@gmail.com>
2023-04-03 15:46:46 -0500

    modificacion en el modelo area y fuente

--- MoPlaneacion/PLAPROYFUEFIN_Entity.cs
+++ MoPlaneacion/PLAPROYFUEFIN_Entity.cs
@@ -19,6 +19,8 @@ namespace MoDLL.MoPlaneacion
         public string CODPROGRAMA { get; set; }
         public string CODPROYECTO { get; set; }
         public string CODIGOFUENTE { get; set; }
+        public string DESFUENTE { get; set; }
+        
         public double VALOR { get; set; }  
         public string CODIGOFUENTE_TEM { get; set; }
         public string NOMBRE{ get; set; }///DESC
