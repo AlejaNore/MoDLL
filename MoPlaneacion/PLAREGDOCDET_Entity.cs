59ae04094814fc740fdf0b84eb4f7c8f50b28874
ALEJANDRA NOREÑA <anm22394@gmail.com>
2023-04-04 12:17:47 -0500

    se agrega el ANOINIVIG en la clase PLAREGDOCENC_Entity y se crea la clase PLAREGDOCDET_Entity

---
+++ MoPlaneacion/PLAREGDOCDET_Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoDLL.MoPlaneacion
+{
+    public class PLAREGDOCDET_Entity
+    {
+        public string Tabla = "PLAREGDOCDET";
+        public int Estado { get; set; }
+        public string CODEMPRESA { get; set; }
+        public string CODSUCURSAL { get; set; }
+        public string CODDOC { get; set; }
+        public string PREFIJO { get; set; }
+        public int CONSEC { get; set; }
+        public int POSICION { get; set; }
+        public int ANOINIVIG { get; set; }
+        public int VERSION { get; set; }
+        public int ANOPLANACC { get; set; }
+        public string CODPLANDES { get; set; }
+        public string CODEJEEST { get; set; }
+        public string CODPROGRAMA { get; set; }
+        public string CODPROYECTO { get; set; }
+        public string CODACTIVIDAD { get; set; }
+        public string CODTAREA { get; set; }
+        public double CANTAVANCE { get; set; }
+        public string EXPLICACION { get; set; }
+    }
+}
