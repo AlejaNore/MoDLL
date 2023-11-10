432d52964d4653c574aafd7c70771b0d4dfc5e37
ORIONALEJANDRA <orion.alejandra.norena@gmail.com>
2023-11-10 15:33:01 -0500

    SE CREAN MAS CAMPOS EN SUCURSAL_Entity

--- MoConfiguracion/SUCURSAL_Entity.cs
+++ MoConfiguracion/SUCURSAL_Entity.cs
@@ -13,6 +13,16 @@ namespace MoDLL
         public string CODEMPRESA { get; set; }
         public string CODSUCURSAL { get; set; }
         public string RAZONSOCIAL { get; set; }
-        
+        public string DIRECCION { get; set; }
+        public string CODPAIS { get; set; }
+        public string CODPROVINCIA { get; set; }
+        public string CODMUNICIPIO { get; set; }
+        public string TELEFONO1 { get; set; }
+        public string TELEFONO2 { get; set; }
+        public string TELEFONO3 { get; set; }
+        public string FAX1 { get; set; }
+        public string WEB_SITE { get; set; }
+        public string EMAIL { get; set; }
+
     }
 }
