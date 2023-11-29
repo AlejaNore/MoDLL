d167c3013ad8ab320caf744991ac93a7f4bc371d
ANDRY <orion.andry.toro@gmail.com>
2023-11-28 19:51:17 -0500

    modiciacion en el seleccionar los parametros encabezado

--- Seleccionar_Entity.cs
+++ Seleccionar_Entity.cs
@@ -16,11 +16,17 @@ namespace MoDLL
         public string CODPARAMETRO3 { get; set; }
         public string DESPARAMETRO3 { get; set; }
         public string CODPARAMETRO4 { get; set; }
+        public string DESPARAMETRO4 { get; set; }
         public string CODPARAMETRO5 { get; set; }
+        public string DESPARAMETRO5 { get; set; }
         public string CODPARAMETRO6 { get; set; }
+        public string DESPARAMETRO6 { get; set; }
         public string CODPARAMETRO7 { get; set; }
+        public string DESPARAMETRO7 { get; set; }
         public string CODPARAMETRO8 { get; set; }
+        public string DESPARAMETRO8 { get; set; }
         public string CODPARAMETRO9 { get; set; }
+        public string DESPARAMETRO9 { get; set; }
 
 
     }
